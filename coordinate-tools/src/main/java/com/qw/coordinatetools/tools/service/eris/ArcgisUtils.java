0719061ef74f40c5c6cbfc8f56320233d9202b84
qiuwei <qiuwei92@hotmail.com>
2020-12-29 20:40:01 +0800

    1.修改手绘地图拉伸的bug

--- coordinate-tools/src/main/java/com/qw/coordinatetools/tools/service/eris/ArcgisUtils.java
+++ coordinate-tools/src/main/java/com/qw/coordinatetools/tools/service/eris/ArcgisUtils.java
@@ -2,9 +2,12 @@ package com.qw.coordinatetools.tools.service.eris;
 
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSON;
 import com.qw.coordinatetools.tools.bean.ExportMapBean;
 
 import java.io.File;
+import java.math.BigDecimal;
+import java.util.Map;
 
 public class ArcgisUtils {
     /**
@@ -21,10 +24,26 @@ public class ArcgisUtils {
         String format = "png32";
         String transparent = "true";
         String layers = "show:" + layerIndex;
-        String layerUrl = serviceUrl + "/export?bbox=" + bbox + "&size=" + size + "&dpi=" + dpi + "&format=" + format + "&transparent=" + transparent + "&layers=" + layers + "&f=image";
+        String layerUrl = serviceUrl + "/export?bbox=" + bbox + "&size=" + size + "&dpi=" + dpi + "&format=" + format + "&transparent=" + transparent + "&layers=" + layers + "&f=pjson";
+        String json = HttpUtil.get(layerUrl);
+        Map resutlMap = JSON.parseObject(json, Map.class);
+        Map extentMap = (Map)resutlMap.get("extent");
+        bean.setMaxX(((BigDecimal)extentMap.get("xmax")).doubleValue());
+        bean.setMaxY(((BigDecimal)extentMap.get("ymax")).doubleValue());
+        bean.setMinX(((BigDecimal)extentMap.get("xmin")).doubleValue());
+        bean.setMinY(((BigDecimal)extentMap.get("ymin")).doubleValue());
         String tempUrl =System.getProperty("java.io.tmpdir");
         String downFile = tempUrl  + "downloadTemp" + File.separator + IdUtil.fastSimpleUUID() + ".png";
-        HttpUtil.downloadFile(layerUrl, downFile);
+        String url = resutlMap.get("href").toString();
+       try{
+           HttpUtil.downloadFile(url, new File(downFile),3000);
+       }catch (Exception e){
+           System.out.println("url:"+url+"  异常重新出图");
+           layerUrl = serviceUrl + "/export?bbox=" + bbox + "&size=" + size + "&dpi=" + dpi + "&format=" + format + "&transparent=" + transparent + "&layers=" + layers + "&f=image";
+           HttpUtil.downloadFile(layerUrl, downFile);
+       }
+
+
         return downFile;
     }
 
